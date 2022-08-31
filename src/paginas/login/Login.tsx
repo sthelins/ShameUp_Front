ff5c6737d8619f83210a3885d35e22e5ac168c5c
igoryouiti <igoryouiti@hotmail.com>
2022-08-31 14:41:38 -0300

    criacao e implementacao do componente login

---
+++ src/paginas/login/Login.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import { Button, Grid, TextField, Typography } from '@material-ui/core'
+import { Box } from '@mui/material'
+import { Link } from 'react-router-dom'
+
+import './Login.css'
+
+function Login() {
+  return (
+    <Grid container direction='row' justifyContent='center' alignItems='center'>
+        <Grid alignItems='center' sm={6}>
+            <Box paddingX={20}>
+                <form action="">
+                    <Typography variant='h3' gutterBottom
+                    color='textPrimary' component='h3'
+                    align='center' className='textos1'
+                    >
+                        Entrar
+                    </Typography>
+                    <TextField id='usuario' label='usuário'
+                    variant='outlined' name='usuario'
+                    type={'email'} margin='normal'
+                    fullWidth />
+                    <TextField id='senha' label='senha'
+                    variant='outlined' name='usuario'
+                    type={'password'} margin='normal'
+                    fullWidth />
+                    <Box marginTop={2} textAlign='center'>
+                        <Link to='/home' className='text-decorator-none'>
+                            <Button type='submit' variant='contained' color='primary'>
+                                Logar
+                            </Button>
+                        </Link>
+                    </Box>
+                </form>
+                <Box display='flex' justifyContent={'center'} marginTop={2}>
+                    <Box marginRight={1}>
+                        <Typography variant='subtitle1' gutterBottom align='center'>Não tem conta?</Typography>
+                    </Box>
+                    <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                </Box>
+            </Box>
+        </Grid>
+        <Grid sm={6} className='imagem'>
+
+        </Grid>
+    </Grid>
+  )
+}
+
+export default Login
