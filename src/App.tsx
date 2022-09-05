6c53071e18a82c4d7791a7b222ae2964aaab48c6
lucas <dantaslucas337@gmail.com>
2022-09-05 09:28:06 -0300

    Alteração dos imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from'./componentes/estaticos/footer/Footer'
 
 function App() {
  return (
-  <h1>hello</h1>
+  <h1> <Footer /> </h1>
  );
 }
 
