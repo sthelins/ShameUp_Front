158d8898ddb5caca83a0e0b25a3c5feeb86e56cd
Pedro <pedrocsca@gmail.com>
2022-09-05 11:46:32 -0300

    implementando função de cadastro

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -7,3 +7,7 @@ export const login = async(url: any, dados: any, setDado: any) => {
     const response = await api.post(url, dados)
     setDado(response.data)
 } 
+export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data)
+}
