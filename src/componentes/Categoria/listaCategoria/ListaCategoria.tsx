b94759ed31c28e748c0c9b01cd069953f4b996c6
Pedro <pedrocsca@gmail.com>
2022-09-12 10:48:07 -0300

    Implementando listacategoria

---
+++ src/componentes/Categoria/listaCategoria/ListaCategoria.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core'
+import { Box } from '@mui/material'
+import './ListaTema.css'
+import { busca } from '../../../services/Service'
+import Categoria from '../../../models/Categoria'
+import useLocalStorage from 'react-use-localstorage';
+
+function ListaCategoria() {
+
+    const [categorias, setCategorias] = useState<Categoria[]>([])
+    const [token, setToken] = useLocalStorage('token');
+    let navigate = useNavigate()
+
+       useEffect(() => {
+        if (token === '') {
+            alert("Você precisa estar logado")
+            navigate("/login")
+        }
+    }, [token])
+
+    async function getCategorias() {
+        await busca("/temas", setCategorias, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    useEffect(() => {
+        getCategorias()
+    }, [categorias.length])
+
+
+    return (
+        <>
+            {
+                categorias.map(categoria => (
+                    <Box m={2} key={categoria.id}>
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>
+                                    Categoria
+                                </Typography>
+                                <Typography variant="h5" component="h2">
+                                    {categoria.descricao}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Box display="flex" justifyContent="center" mb={1.5} >
+                                    <Link to={`/formularioTema/${categoria.id}`} className="text-decorator-none">
+                                        <Box mx={1}>
+                                            <Button variant="contained" className="marginLeft" size='small' color="primary">
+                                                atualizar
+                                            </Button>
+                                        </Box>
+                                    </Link>
+                                    <Link to={`/deletarTema/${categoria.id}`} className="text-decorator-none">
+                                        <Box mx={1}>
+                                            <Button variant="contained" size='small' color="secondary">
+                                                deletar
+                                            </Button>
+                                        </Box>
+                                    </Link>
+                                </Box>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                ))
+            }
+        </>
+    );
+
+}
+export default ListaCategoria;
