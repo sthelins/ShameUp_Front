37d54ef00fc4045fe8067779c3ae8618bafd2377
Samy de Simoni <sssouza.samy@gmail.com>
2022-09-12 10:38:08 -0300

    alterações nas rotas (app.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,34 @@
-import React from 'react';
-import './App.css';
-import Navbar from './componentes/estaticos/navbar/Navbar';
-import Home from './paginas/home/Home';
-import Login from './paginas/login/Login';
-import Sobrenos from './paginas/sobrenos/Sobrenos';
-import Footer from './componentes/estaticos/footer/Footer';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
-import ListaCategoria from './componentes/Categoria/listaCategoria/ListaCategoria';
-import CadastroCategoria from './componentes/Categoria/cadastroCategoria/CadastroCategoria';
-import DeletarCategoria from './componentes/Categoria/deletarCadastro/DeletarCategoria';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./componentes/estaticos/navbar/Navbar";
+import Home from "./paginas/home/Home";
+import Login from "./paginas/login/Login";
+import Sobrenos from "./paginas/sobrenos/Sobrenos";
+import Footer from "./componentes/estaticos/footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
+import ListaCategoria from "./componentes/Categoria/listaCategoria/ListaCategoria";
+import CadastroCategoria from "./componentes/Categoria/cadastroCategoria/CadastroCategoria";
+import DeletarCategoria from "./componentes/Categoria/deletarCategoria/DeletarCategoria";
 
 function App() {
   return (
     <Router>
       <Navbar />
-        <div style={{minHeight: '100vh'}} >
+      <div style={{ minHeight: "100vh" }}>
         <Routes>
-          <Route path= '/home' element={<Home />}  />
-          <Route path="/Categoria" element={<ListaCategoria />} />
-          <Route path= '/' element={<Login />}  />
-          <Route path='/cadastrousuario' element={<CadastroUsuario />} />
-          <Route path= '/login' element={<Login />}  />
-          <Route path= '/sobre' element={<Sobrenos />}  />
+          <Route path="/home" element={<Home />} />
+          <Route path="/categorias" element={<ListaCategoria />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sobre" element={<Sobrenos />} />
           <Route path="/formularioCategoria" element={<CadastroCategoria />} />
-
-
-<Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
-
-<Route path="/deletarTema/:id" element={<DeletarCategoria />} />
-
+          <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
         </Routes>
-        </div>
+      </div>
       <Footer />
     </Router>
-    
   );
 }
 
