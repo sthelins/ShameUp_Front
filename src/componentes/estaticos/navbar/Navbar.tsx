95ea7070ef2b66124591a3a261c7d1ee071a0f60
igoryouiti <igoryouiti@hotmail.com>
2022-09-12 11:55:42 -0300

    Adição de botão para rotas de categorias

--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 import './Navbar.css'
 
 import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import useLocalStorage from 'react-use-localstorage'
 
 function Navbar() {
+
+  const [token, setToken] = useLocalStorage('token')
+  let navigate = useNavigate()
+
+  function goLogout() {
+    setToken('')
+    alert("Usuário deslogado")
+    navigate('/login')
+  }
+
+
   return (
     <>
       <AppBar position="static" className="appBar">
@@ -37,6 +49,22 @@ function Navbar() {
               </Box>
 
               <Box mx={2} className="cursor">
+                <Link to="/formularioCategoria" className="text-decorator">
+                  <Typography variant="h6" color="inherit">
+                    Cadastrar Tema
+                  </Typography>
+                </Link>
+              </Box>
+
+              <Box mx={2} className="cursor">
+                <Link to="/categorias" className="text-decorator">
+                  <Typography variant="h6" color="inherit">
+                    Temas
+                  </Typography>
+                </Link>
+              </Box>
+
+              <Box mx={2} className="cursor">
                 <Link to="/cadastrousuario" className="text-decorator">
                   <Typography variant="h6" color="inherit">
                     Cadastro
@@ -44,7 +72,7 @@ function Navbar() {
                 </Link>
               </Box>
 
-              <Box mx={2} className="cursor">
+              <Box mx={2} className="cursor" onClick={goLogout}>
                 <Link to="/login" className="text-decorator">
                   <Typography variant="h6" color="inherit">
                     logout
