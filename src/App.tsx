075a2b1a124e0fd47580e7ea0ba4002af3cf007a
Sthefany <sthefanylins_albuquerque@hotmail.com>
2022-09-14 10:10:02 -0300

    commit vazio

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import "./App.css";
-import Navbar from "./componentes/estaticos/navbar/Navbar";
-import Home from "./paginas/home/Home";
-import Login from "./paginas/login/Login";
-import Sobrenos from "./paginas/sobrenos/Sobrenos";
-import Footer from "./componentes/estaticos/footer/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
-import ListaCategoria from "./componentes/categoria/listaCategoria/ListaCategoria";
-import CadastroCategoria from "./componentes/categoria/cadastroCategoria/CadastroCategoria";
-import DeletarCategoria from "./componentes/categoria/deletarCategoria/DeletarCategoria";
-import CadastroPostagem from "./componentes/categoria/cadastroPostagem/CadastroPostagem";
-import ListarPostagem from "./componentes/categoria/listarPostagem/ListarPostagem";
-import DeletarPostagem from "./componentes/categoria/deletarPostagem/DeletarPostagem";
+/*import React from 'react'
+import './App.css'
+import Navbar from './componentes/estaticos/navbar/Navbar'
+import Home from './paginas/home/Home'
+import Login from './paginas/login/Login'
+import Sobrenos from './paginas/sobrenos/Sobrenos'
+import Footer from './componentes/estaticos/footer/Footer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario'
+import ListaCategoria from './componentes/categoria/listaCategoria/ListaCategoria'
+import CadastroCategoria from './componentes/categoria/cadastroCategoria/CadastroCategoria'
+import DeletarCategoria from './componentes/categoria/deletarCategoria/DeletarCategoria'
+import CadastroPostagem from './componentes/postagens/cadastroPostagem/CadastroPostagem'
+import ListarPostagem from './componentes/postagens/listarPostagem/ListarPostagem'
+import DeletarPostagem from './componentes/postagens/deletarPostagem/DeletarPostagem'
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div style={{ minHeight: "100vh" }}>
+      <div style={{ minHeight: '100vh' }}>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/categorias" element={<ListaCategoria />} />
@@ -28,16 +28,22 @@ function App() {
           <Route path="/sobre" element={<Sobrenos />} />
           <Route path="/postagens" element={<ListarPostagem />} />
           <Route path="/formularioPostagem" element={<CadastroPostagem />} />
-          <Route path="/formularioPostagem/:id" element={<CadastroPostagem />} />
+          <Route
+            path="/formularioPostagem/:id"
+            element={<CadastroPostagem />}
+          />
           <Route path="/formularioCategoria" element={<CadastroCategoria />} />
-          <Route path="/formularioCategoria/:id" element={<CadastroCategoria />} />
+          <Route
+            path="/formularioCategoria/:id"
+            element={<CadastroCategoria />}
+          />
           <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
           <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
         </Routes>
       </div>
       <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App*/
