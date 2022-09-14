6727cb276479af1bce00575e4335665834816f4f
Sthefany <sthefanylins_albuquerque@hotmail.com>
2022-09-14 10:51:05 -0300

    resolução conflito ModalPostagem

--- src/componentes/postagens/modalPostagem/ModalPostagem.tsx
+++ src/componentes/postagens/modalPostagem/ModalPostagem.tsx
@@ -1,13 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
-import Modal from '@mui/material/Modal'
-import { Button } from '@material-ui/core'
-import CloseIcon from '@material-ui/icons/Close'
-import './ModalPostagem.css'
-import { Box } from '@mui/material'
-import CadastroPostagem from './ca'
-=======
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Modal from '@mui/material/Modal';
 import {Button} from "@material-ui/core"
