6b796307b063bbaf999436fe09ee3f3a6f5b44f3
igoryouiti <igoryouiti@hotmail.com>
2022-09-15 11:16:02 -0300

    implementacao de redux com id de usuario

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,7 +4,7 @@ export const api = axios.create({ baseURL: "https://shameup.herokuapp.com" });
 
 export const login = async (url: any, dados: any, setDado: any) => {
   const response = await api.post(url, dados);
-  setDado(response.data.token);
+  setDado(response.data);
 };
 export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
   const resposta = await api.post(url, dados);
