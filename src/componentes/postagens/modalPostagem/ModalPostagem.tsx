78f5565f72f021f165aed33f62d1119736c17d23
Samy de Simoni <sssouza.samy@gmail.com>
2022-09-16 17:34:42 -0300

    alterações na estilização do modal

--- src/componentes/postagens/modalPostagem/ModalPostagem.tsx
+++ src/componentes/postagens/modalPostagem/ModalPostagem.tsx
@@ -1,48 +1,48 @@
-import React from 'react'
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
-import Modal from '@mui/material/Modal'
-import { Button } from '@material-ui/core'
-import CloseIcon from '@material-ui/icons/Close'
-import './ModalPostagem.css'
-import { Box } from '@mui/material'
-import CadastroPostagem from '../cadastroPostagem/CadastroPostagem'
+import React from "react";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Modal from "@mui/material/Modal";
+import { Button } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import "./ModalPostagem.css";
+import { Box } from "@mui/material";
+import CadastroPostagem from "../cadastroPostagem/CadastroPostagem";
 
 function getModalStyle() {
-  const top = 50
-  const left = 50
+  const top = 50;
+  const left = 50;
 
   return {
     top: `${top}%`,
     left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`
-  }
+    transform: `translate(-${top}%, -${left}%)`,
+  };
 }
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     paper: {
-      position: 'absolute',
+      position: "absolute",
       width: 400,
       backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
+      border: "2px solid #000",
       boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3)
-    }
+      padding: theme.spacing(2, 4, 3),
+    },
   })
-)
+);
 
 function ModalPostagem() {
-  const classes = useStyles()
-  const [modalStyle] = React.useState(getModalStyle)
-  const [open, setOpen] = React.useState(false)
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
-    setOpen(true)
-  }
+    setOpen(true);
+  };
 
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
   const body = (
     <div style={modalStyle} className={classes.paper}>
@@ -52,7 +52,7 @@ function ModalPostagem() {
 
       <CadastroPostagem />
     </div>
-  )
+  );
 
   return (
     <div>
@@ -64,10 +64,11 @@ function ModalPostagem() {
         onClose={handleClose}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
+        className="classe"
       >
         {body}
       </Modal>
     </div>
-  )
+  );
 }
-export default ModalPostagem
+export default ModalPostagem;
