6a5798755b2c4c9f3e904ddc600f304a4ade2a88
Sthefany <sthefanylins_albuquerque@hotmail.com>
2022-09-19 09:12:12 -0300

    aceitando alteração da main

--- src/componentes/postagens/comentarioPostagem/ComentarioPostagem.tsx
+++ src/componentes/postagens/comentarioPostagem/ComentarioPostagem.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import './ComentarioPostagem.css'
-
-=======
-import React from "react";
-import "./ComentarioPostagem.css";
-import {Typography } from "@mui/material";
->>>>>>> 7b089bedc099be3bbbfa40dd2d05edfbdb0ff0b9:src/componentes/postagens/comentarioPostagem/ComentarioPostagem.tsx
+import { Typography } from '@mui/material'
 interface ComentarioProps {
   conteudo: string
 }
 
 function ComentarioPostagem({ conteudo }: ComentarioProps) {
-<<<<<<< HEAD:src/componentes/postagens/comentarioPostagem/comentarioPostagem.tsx
-  return <p>{conteudo}</p>
+  return <p className="conteudo-comentario">{conteudo}</p>
 }
 
 export default ComentarioPostagem
